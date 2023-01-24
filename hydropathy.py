750088d0fc7a877a2a31ac9ebc8cada391da63fa
icacedo <icacedo@ucdavis.edu>
2023-01-24 15:27:30 -0800

    maybe done

--- hydropathy.py
+++ hydropathy.py
@@ -11,6 +11,29 @@ parser.add_argument("-window_size", help="size of sliding window given in n \
 	bases", type=int, required=True)
 args = parser.parse_args()
 
+scores = {
+	"I": 4.5,
+	"V": 4.2,
+	"L": 3.8,
+	"F": 2.8,
+	"C": 2.5,
+	"M": 1.9,
+	"A": 1.8,
+	"G": -0.4,
+	"T": -0.7,
+	"S": -0.8,
+	"W": -0.9,
+	"Y": -1.3,
+	"P": -1.6,
+	"H": -3.2,
+	"E": -3.5,
+	"Q": -3.5,
+	"D": -3.5,
+	"N": -3.5,
+	"K": -3.9,
+	"R": -4.5
+}
+
 n = args.window_size
 
 seq = ''
@@ -26,7 +49,30 @@ for i in range(len(seq)):
 	if len(seq[i:n+i]) == n:
 		win = seq[i:n+i]
 	else: continue
-	print(win)
+	total = 0
+	for aa in win:
+		total += scores[aa]
+	avg = total/n
+	# literally need tabs?
+	print(i+1, win, "%.1f" % avg)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
