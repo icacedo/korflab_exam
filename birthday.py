ee4753aed247a5cd14a5b6050fcf68b7409004f8
icacedo <icacedo@ucdavis.edu>
2023-01-24 14:05:56 -0800

    done

--- birthday.py
+++ birthday.py
@@ -4,16 +4,47 @@ import random
 parser = argparse.ArgumentParser()
 parser.add_argument("class_size", help="number of students in a hypothetical \
 	classroom", type=int)
+parser.add_argument("n_sims", help="number of simulations to run", type=int)
 args = parser.parse_args()
 
 class_size = args.class_size
+n_sims = args.n_sims
 
 # only need ONE PAIR
 
-npairs = int((class_size*(class_size-1))/2)
+# what does "solve analytically" mean?
+def birthday_sim(class_size):
+	students = [random.randint(1,365) for x in range(class_size)]
 
-students = [random.randint(1,365) for x in range(class_size)]
+	for i in range(class_size):
+		for j in range(i, class_size):
+			if i == j: continue
+			elif students[i] == students[j]:
+				yield 1 #, students[i], students[j]
+			else:
+				yield 0 #, students[i], students[j]
 
-print(students)
+# probability of AT LEAST two
+
+matches = 0
+for i in range(n_sims):
+	prev = 0
+	for i in birthday_sim(class_size):
+		if i == 1 and prev == 0:
+			# check tuples
+			#print(i[0], i[1], i[2])
+			matches += 1
+			prev += 1
+
+print("prob of same birthday: ", matches/n_sims)
+
+
+
+
+
+
+
+
+
 
 
