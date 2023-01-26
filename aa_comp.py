fee3dae7eecf0b45e1a0af78469597b3069f34fb
icacedo <icacedo@ucdavis.edu>
2023-01-26 13:48:18 -0800

    done

---
+++ aa_comp.py
@@ -0,0 +1,31 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("fasta", type=argparse.FileType('r'))
+args = parser.parse_args()
+
+fasta = args.fasta
+
+# only works with uppercase letters ACTG
+seq = ''
+for line in fasta.readlines():
+	if line.startswith('>'): continue
+	line = line.rstrip()
+	seq += line
+
+aa_comp = {
+	'A': 0,
+	'C': 0,
+	'T': 0,
+	'G': 0
+}
+
+total = 0
+for aa in seq:
+	aa_comp[aa] += 1
+	total += 1
+
+for aa in aa_comp:
+	aa_comp[aa] = round(aa_comp[aa]/total, 2)
+
+print(aa_comp)
